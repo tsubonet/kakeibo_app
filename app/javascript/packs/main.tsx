2cba1ad1bde5db67a6c160de3b4b651ede38dbba
tsubonet <tsubonet@gmail.com>
2018-02-18 21:09:33 +0800

    to typescript

--- app/javascript/packs/main.tsx
+++ app/javascript/packs/main.tsx
@@ -3,29 +3,22 @@ import * as React from 'react'
 import { Provider } from 'react-redux'
 import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 import ReactOnRails from 'react-on-rails'
-
 import createHistory from 'history/createBrowserHistory'
-
 import { BrowserRouter } from 'react-router-dom'
 import RouterContainer from '../containers/router_container'
-
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
-
-import createSagaMiddleware from 'redux-saga'
-
-// Create a history of your choosing (we're using a browser history in this case)
-const history = createHistory()
-
-// Build the middleware for intercepting and dispatching navigation actions
-const middleware = routerMiddleware(history)
 
 import actionPath from '../reducers/action_path'
 import date from '../reducers/date'
 import records from '../reducers/records'
 import record from '../reducers/record'
 
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
 import mySaga from '../sagas'
+
+const history = createHistory()
+const middleware = routerMiddleware(history)
 
 const App = (props, railsContext) => {
   const sagaMiddleware = createSagaMiddleware()

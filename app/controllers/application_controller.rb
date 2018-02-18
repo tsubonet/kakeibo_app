1b9e43bf23c4c17ef8cf80cb3497636803db80ee
tsubonet <tsubonet@gmail.com>
2018-02-18 18:01:04 +0800

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,39 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def action_path
+    "#{controller_path}##{action_name}"
+  end
+
+  def common_props
+    {
+      actionPath: action_path,
+      #currentUser: current_user,
+    }
+  end
+
+  def render_for_react(props: {}, status: 200)
+    if request.format.json?
+      response.headers["Cache-Control"] = "no-cache, no-store"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+      response.headers["Pragma"] = "no-cache"
+      render(
+        json: common_props.merge(props),
+        status: status,
+      )
+    else
+      render(
+        html: view_context.react_component(
+          "App",
+          prerender: false,
+          props: common_props.merge(props).as_json,
+        ),
+        layout: true,
+        status: status,
+      )
+    end
+  end
+
 end

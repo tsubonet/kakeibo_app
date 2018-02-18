c9b88a038986b3809d67d441f2389282d2138ae5
tsubonet <tsubonet@gmail.com>
2018-02-18 16:24:42 +0800

    change typescript

--- app/javascript/components/calendar.tsx
+++ app/javascript/components/calendar.tsx
@@ -1,45 +1,36 @@
-import PropTypes from 'prop-types';
-import * as React from "react";
+import PropTypes from 'prop-types'
+import * as React from 'react'
 
 export default class HelloWorld extends React.Component<any, any> {
   static propTypes = {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
+  }
 
   /**
    * @param props - Comes from your rails view.
    */
   constructor(props) {
-    super(props);
+    super(props)
 
     // How to set initial state in ES6 class syntax
     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
+    this.state = { name: this.props.name }
   }
 
-  updateName = (name) => {
-    this.setState({ name });
-  };
+  updateName = name => {
+    this.setState({ name })
+  }
 
   render() {
     return (
       <div>
-        <h3>
-          Hello, {this.state.name}!
-        </h3>
+        <h3>Hello, {this.state.name}!</h3>
         <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
+        <form>
+          <label htmlFor="name">Say hello to:</label>
+          <input id="name" type="text" value={this.state.name} onChange={e => this.updateName(e.target.value)} />
         </form>
       </div>
-    );
+    )
   }
 }

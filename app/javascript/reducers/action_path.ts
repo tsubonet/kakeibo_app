c8bb204c04830c26914d561b6c9dcad7b0ecd939
tsubonet <tsubonet@gmail.com>
2018-02-19 21:19:06 +0800

    to typescript

--- app/javascript/reducers/action_path.ts
+++ app/javascript/reducers/action_path.ts
@@ -1,4 +1,4 @@
-export default function reducer(state = '', action) {
+export default function reducer(state: string = '', action): string {
   switch (action.type) {
     case 'GET_ACTION_PATH':
       return action.actionPath

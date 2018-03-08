ed42d81a9dcc204af319d4c94878e8ce0557b2e6
tsubonet <tsubonet@gmail.com>
2018-03-08 14:03:32 +0900

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   private
 

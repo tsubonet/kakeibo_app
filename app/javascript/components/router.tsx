0ea6f2521b0cc642e16b2ce06d3c0a0b5fe11278
tsubonet <tsubonet@gmail.com>
2018-03-21 23:50:34 +0900

    auth

--- app/javascript/components/router.tsx
+++ app/javascript/components/router.tsx
@@ -36,6 +36,10 @@ export default class Router extends React.Component<Props> {
 
   componentDidMount() {
     const { auth, transitTo } = this.props
+
+    const url: string = document.location.href
+    transitTo(url, auth)
+
     window.addEventListener('popstate', () => {
       const url: string = document.location.href
       transitTo(url, auth)
--- app/javascript/packs/main.tsx
+++ app/javascript/packs/main.tsx
@@ -8,21 +8,23 @@ import { BrowserRouter } from 'react-router-dom'
 import Router from '../containers/router'
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux'
 import persistState from 'redux-localstorage'
+import createSagaMiddleware from 'redux-saga'
+import mySaga from '../sagas'
 
 import auth from '../reducers/auth'
 import date from '../reducers/date'
 import records from '../reducers/records'
 import recordsYear from '../reducers/records_year'
 
-import logger from 'redux-logger'
-import createSagaMiddleware from 'redux-saga'
-import mySaga from '../sagas'
-
 const history = createHistory()
 
 const App = (props, railsContext) => {
   const sagaMiddleware = createSagaMiddleware()
-  const middlewares = [routerMiddleware(history), sagaMiddleware, logger]
+  const middlewares = [routerMiddleware(history), sagaMiddleware]
+  if (process.env.NODE_ENV === 'development') {
+    const { logger } = require('redux-logger')
+    middlewares.push(logger)
+  }
   const rootReducer = combineReducers({
     auth,
     date,

b3596f0e9fdce482db278adbeb7aa2806c0b6b17
tsubonet <tsubonet@gmail.com>
2018-03-30 08:46:24 +0900

    tslint

--- app/javascript/utils.ts
+++ app/javascript/utils.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { css, keyframes } from 'styled-components'
 
 function getHoliday(h) {
-  var g = [
+  let g = [
     { date: '1/1', startY: 1949, endY: 9999 },
     { date: '1/15', startY: 1949, endY: 1999 },
     { date: '1/' + happyMonday(h, 1, 2), startY: 2000, endY: 9999 },
@@ -26,22 +26,22 @@ function getHoliday(h) {
     { date: '11/23', startY: 1948, endY: 9999 },
     { date: '12/23', startY: 1989, endY: 9999 },
   ]
-  var b = []
-  var f = 0
-  for (var d = 0; d < g.length; d++) {
+  let b = []
+  let f = 0
+  for (let d = 0; d < g.length; d++) {
     if (h >= g[d]['startY'] && h <= g[d]['endY']) {
-      var a = g[d]['date'].split('/')
+      let a = g[d]['date'].split('/')
       b[f] = { month: a[0], day: a[1] }
       f++
-      var e = substituteDay(h, parseFloat(a[0]), parseFloat(a[1]), g)
+      let e = substituteDay(h, parseFloat(a[0]), parseFloat(a[1]), g)
       if (e) {
-        var c = e.split('/')
+        let c = e.split('/')
         b[f] = { month: c[0], day: c[1] }
         f++
       }
-      var j = nationalHoliday(h, parseFloat(a[0]), parseFloat(a[1]), g)
+      let j = nationalHoliday(h, parseFloat(a[0]), parseFloat(a[1]), g)
       if (j) {
-        var c = j.split('/')
+        let c = j.split('/')
         b[f] = { month: c[0], day: c[1] }
         f++
       }
@@ -81,7 +81,7 @@ function shunbun(a) {
       }
       return 20
   }
-  //return day
+  // return day
 }
 function shubun(a) {
   if (a < 1900 || a > 2099) {
@@ -117,24 +117,25 @@ function shubun(a) {
   }
 }
 function happyMonday(d, e, c) {
-  var a = new Date(d, e - 1, 1).getDay()
+  let a = new Date(d, e - 1, 1).getDay()
+  let b
   if (a <= 1) {
-    var b = 2 - a
+    b = 2 - a
   } else {
-    var b = 9 - a
+    b = 9 - a
   }
   return b + 7 * (c - 1)
 }
 function substituteDay(f, h, c, g) {
   if (f >= 1973) {
-    var d = new Date(f, h - 1, c)
-    var b = d.getDay()
+    let d = new Date(f, h - 1, c)
+    let b = d.getDay()
     if (b === 0) {
-      var a = false
+      let a = false
       while (!a) {
         a = true
         d.setDate(d.getDate() + 1)
-        for (var e = 0; e < g.length; e++) {
+        for (let e = 0; e < g.length; e++) {
           if (
             d.getFullYear() >= g[e]['startY'] &&
             d.getFullYear() <= g[e]['endY'] &&
@@ -151,10 +152,10 @@ function substituteDay(f, h, c, g) {
 }
 function nationalHoliday(e, g, b, f) {
   if (e >= 1988) {
-    var c = new Date(e, g - 1, b)
+    let c = new Date(e, g - 1, b)
     c.setDate(c.getDate() + 2)
-    var a = false
-    for (var d = 0; d < f.length; d++) {
+    let a = false
+    for (let d = 0; d < f.length; d++) {
       if (
         c.getFullYear() >= f[d]['startY'] &&
         c.getFullYear() <= f[d]['endY'] &&
@@ -166,7 +167,7 @@ function nationalHoliday(e, g, b, f) {
     if (a) {
       c.setDate(c.getDate() - 1)
       a = false
-      for (var d = 0; d < f.length; d++) {
+      for (let d = 0; d < f.length; d++) {
         if (
           c.getFullYear() >= f[d]['startY'] &&
           c.getFullYear() <= f[d]['endY'] &&

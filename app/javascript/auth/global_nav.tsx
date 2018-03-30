6c94e66521ffb7b4e5aed534d9036ec3bae5dfc7
tsubonet <tsubonet@gmail.com>
2018-03-30 19:59:04 +0900

    add custom link

--- app/javascript/auth/global_nav.tsx
+++ app/javascript/auth/global_nav.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import { Route } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import Link from '../components/link'
 import { signout } from '../actions/auth'
@@ -10,18 +12,17 @@ interface Props {
 }
 class GlobalNav extends React.Component<Props> {
   signout(e) {
-    const { auth } = this.props
+    const { auth, dispatch } = this.props
     e.preventDefault()
-    this.props.dispatch(signout(auth))
+    dispatch(signout(auth))
   }
-
   render() {
     const { auth } = this.props
     return (
       <div>
         <div>
-          {!auth.isAuthenticated && <Link href="/signup">新規登録</Link>}{' '}
-          {!auth.isAuthenticated && <Link href="/login">ログイン</Link>}
+          {!auth.isAuthenticated && <MenuLink to="/signup" label="新規登録" />}{' '}
+          {!auth.isAuthenticated && <MenuLink to="/login" label="ログイン" />}
           {auth.isAuthenticated && (
             <a href="#" onClick={this.signout.bind(this)}>
               ログアウト
@@ -33,8 +34,26 @@ class GlobalNav extends React.Component<Props> {
   }
 }
 
+interface MenuProps {
+  label: string
+  to: string
+  activeOnlyWhenExact?: boolean
+}
+const MenuLink = ({ label, to, activeOnlyWhenExact }: MenuProps) => (
+  <Route
+    path={to}
+    exact={activeOnlyWhenExact}
+    children={({ match }) => (
+      <div className={match ? 'active' : ''}>
+        {match ? '> ' : ''}
+        <Link href={to}>{label}</Link>
+      </div>
+    )}
+  />
+)
+
 function mapStateToProps({ auth }: StoreState) {
   return { auth }
 }
 
-export default connect(mapStateToProps)(GlobalNav)
+export default withRouter(connect(mapStateToProps)(GlobalNav))
